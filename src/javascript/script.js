d9bd01d6335e42c6a48c51b53052613d43a5012e
Dário Alef <dario.alef@gmail.com>
2025-01-04 09:34:40 -0400

    ?

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,1 +1,7 @@
-"https://youtu.be/8V3mw1w6h0U?si=XY6CFdQvsxrKZ0rV"
+// https://youtu.be/8V3mw1w6h0U?si=XY6CFdQvsxrKZ0rV
+
+$(document).ready(function () {
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+    });
+});
